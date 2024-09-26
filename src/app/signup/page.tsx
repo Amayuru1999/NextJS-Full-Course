9a0ce9d2026e9edd2647b3028651afa2899d9d64
Amayuru1999 <amarasingheau@gmail.com>
2024-09-26 22:51:42 +0530

    feat: Added database connection with mongodb #3

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -18,13 +18,37 @@ export default function SignUpPage() {
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>SignUp</h1>
             <hr/>
-            <label htmlFor="username">username</label>
+            <label htmlFor="username">Username</label>
             <input
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
                 id="username"
                 type="text"
                 value={user.username}
-                onCanPlay={(e) => setUser({...user, username: e.target.value})}
+                onChange={(e) => setUser({...user, username: e.target.value})}
+                placeholder="Username"
             />
+            <label htmlFor="username">Email</label>
+            <input
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+                placeholder="Email"
+            />
+            <label htmlFor="username">Password</label>
+            <input
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+                id="password"
+                type="text"
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
+                placeholder="Password"
+            />
+            <button className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600' onClick={onSignUp}>
+                Sign Up
+            </button>
+
         </div>
     )
 }
