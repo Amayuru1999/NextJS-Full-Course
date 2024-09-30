36a009b0eef3a8cd406bce4e8b02f2b04466bc42
Amayuru1999 <amarasingheau@gmail.com>
2024-09-30 17:32:50 +0530

    feat: Added Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,44 @@
-export default function LoginPage(){
-    return(
-        <div>
-            <h1>Login Page</h1>
+'use client'
+
+import {useState} from "react";
+
+export default function LoginPage() {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+
+    const onLogin = async () => {
+
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>SignUp</h1>
+            <hr/>
+            <label htmlFor="username">Email</label>
+            <input
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+                placeholder="Email"
+            />
+            <label htmlFor="username">Password</label>
+            <input
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+                id="password"
+                type="text"
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
+                placeholder="Password"
+            />
+            <button className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600' onClick={onLogin}>
+                Sign Up
+            </button>
+
         </div>
     )
 }
