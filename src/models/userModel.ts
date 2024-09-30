667df46f3b947163d55afb2bc8e78f948ca198b4
Amayuru1999 <amarasingheau@gmail.com>
2024-09-30 18:18:12 +0530

    feat: Added user model userModel.ts #1

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
 });
+
+const User = mongoose.model.users || mongoose.model('users',userSchema);
+
+export default User;
