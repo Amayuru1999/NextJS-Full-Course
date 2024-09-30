5f818fbb0dd87178e9fffda8af8f422240dbc4b3
Amayuru1999 <amarasingheau@gmail.com>
2024-09-30 18:23:19 +0530

    feat: Added user model userModel.ts #2

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,14 +3,22 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, 'Please provide a username'],
         unique: true
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please provide an email'],
         unique: true
     },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password']
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    }
 });
 
 const User = mongoose.model.users || mongoose.model('users',userSchema);
